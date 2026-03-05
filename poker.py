41ed1cf19527e436c13ceeed990e800694002124
Antoine Huchard <ahuchard@gmail.com>
2026-03-05 14:40:26 +0100

    add Hand class

--- poker.py
+++ poker.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Card:
     VALUES = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, 
               "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
@@ -12,3 +14,55 @@ class Card:
 
     def __repr__(self):
         return f"{self.value}{self.suit}"
+
+class Hand:
+    def __init__(self, cards):
+        self.cards = sorted(cards, key=lambda c: c.value, reverse=True)
+        counts = Counter(c.value for c in self.cards)
+        self.sorted_counts = sorted(counts.items(), key=lambda x: (x[1], x[0]), reverse=True)
+        
+        self.category_rank = 0
+        self.tie_breakers = []
+        self._evaluate()
+
+    @classmethod
+    def from_string(cls, s):
+        return cls([Card(c) for c in s.split()])
+
+    def _evaluate(self):
+        values = sorted(list(set(c.value for c in self.cards)), reverse=True)
+        freqs = [item[1] for item in self.sorted_counts]
+        
+        is_straight = False
+        straight_high_card = 0
+        
+        if len(values) >= 5:
+            if values[0] - values[4] == 4:
+                is_straight = True
+                straight_high_card = values[0]
+            elif set([14, 5, 4, 3, 2]).issubset(set(values)):
+                is_straight = True
+                straight_high_card = 5
+
+        if is_straight:
+            self.category_rank = 4
+            if straight_high_card == 5:
+                self.tie_breakers = [5, 4, 3, 2, 1]
+            else:
+                self.tie_breakers = list(range(straight_high_card, straight_high_card - 5, -1))
+        
+        elif freqs == [4, 1]: self.category_rank = 7
+        elif freqs == [3, 2]: self.category_rank = 6
+        elif freqs == [3, 1, 1]: self.category_rank = 3
+        elif freqs == [2, 2, 1]: self.category_rank = 2
+        elif freqs == [2, 1, 1, 1]: self.category_rank = 1
+        else: self.category_rank = 0 
+
+        if not self.tie_breakers:
+            self.tie_breakers = [item[0] for item in self.sorted_counts]
+
+    def _comparison_tuple(self):
+        return (self.category_rank, *self.tie_breakers)
+
+    def __gt__(self, other):
+        return self._comparison_tuple() > other._comparison_tuple()
--- test_poker.py
+++ test_poker.py
@@ -1,4 +1,5 @@
 from poker import Card
+from poker import Hand
 
 def test_card_creation():
     c = Card("As")
@@ -9,3 +10,14 @@ def test_card_comparison():
     c1 = Card("Kh") 
     c2 = Card("2d")
     assert c1 > c2
+
+def test_straight_normal():
+
+    h_straight = Hand.from_string("9s 8h 7d 6c 5s")
+    assert h_straight.category_rank == 4
+    assert h_straight._comparison_tuple() == (4, 9, 8, 7, 6, 5)
+
+def test_straight_ace_low_wheel():
+    h_wheel = Hand.from_string("As 2h 3d 4c 5s")
+    assert h_wheel.category_rank == 4
+    assert h_wheel._comparison_tuple() == (4, 5, 4, 3, 2, 1)
